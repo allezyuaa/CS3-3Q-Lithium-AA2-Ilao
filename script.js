eaa94cc2f64b2b089aec6e05af416e3dc60d1af8
Kezya <kjpilao22@src.pshs.edu.ph>
2025-02-14 07:56:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function expoPopuGrowth() {
+    let initialPopulation = parseFloat(prompt('Enter intial population growth:'));
+    let rateGrowth = parseFloat(prompt('Enter rate growth:'));
+    let time = parseFloat(prompt('Enter time:'));
+
+    let finalPopulationGrowth = Math.round(initialPopulation*(Math.E**(rateGrowth*time)))
+
+    let regionOfMonster = prompt('Enter region of monster:');
+    let monsterName = prompt('Enter name of monster:');
+
+    let monsterInformation = monsterNmae.concat(" ", monsterNmae).toUpperCase();
+
+    document.getElementById('result').innerHTML = `After ${time} hours, the population of ${monsterName} has risen to ${finalPopulationGrowth}!`;
+
+}
