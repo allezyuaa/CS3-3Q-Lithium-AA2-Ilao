d07be0ef5a1087c65def317c62bcc241d7268981
Kezya <kjpilao22@src.pshs.edu.ph>
2025-02-14 21:12:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function expoPopuGrowth() {
     let initialPopulation = parseFloat(prompt('Enter intial population growth:'));
     let rateGrowth = parseFloat(prompt('Enter rate growth:'));
     let time = parseFloat(prompt('Enter time:'));
-
-    let finalPopulationGrowth = Math.round(initialPopulation*(Math.exp(rateGrowth*time)))
+    
+    let finalPopulationGrowth = Math.round(initialPopulation*(Math.E**(rateGrowth*time)));
 
     let regionOfMonster = prompt('Enter region of monster:');
     let monsterName = prompt('Enter name of monster:');
